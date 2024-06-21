45a483d789f77f906a745e5e42bc264da76126d7
Anna Shatokhina <anutik_94.08@mail.ru>
2024-06-21 10:34:01 +0300

    добавлены замечания

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -1,10 +1,10 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.qameta.allure.internal.shadowed.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Data
 @AllArgsConstructor
@@ -13,6 +13,16 @@ import lombok.NoArgsConstructor;
 public class User {
     private String name;
     private String job;
+
+    @JsonProperty("per_page")
+    private int perPage;
+
+    // другие поля, геттеры и сеттеры
+
+    public User(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
 }
 
 
--- src/test/java/models/UserResponse.java
+++ src/test/java/models/UserResponse.java
@@ -1,6 +1,7 @@
 package models;
 
-import io.qameta.allure.internal.shadowed.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserResponse {
+
     private String name;
     private String job;
+    @JsonProperty("id") // Добавьте аннотацию для поля "id"
     private int id;
     private String createdAt;
 }
